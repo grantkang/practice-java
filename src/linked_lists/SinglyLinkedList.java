04c3ae014d37391f957346d3a9e85f56ecac7868
Grant Kang <3s.grantkang@gmail.com>
2020-05-30 12:11:15 -0700

    wrote empty/nonempty constructors & getSize/isEmpty methods

--- src/linked_lists/SinglyLinkedList.java
+++ src/linked_lists/SinglyLinkedList.java
@@ -1,6 +1,26 @@
 package linked_lists;
 
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
+    private Node<T> head;
+    private int size;
+
+    public SinglyLinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public SinglyLinkedList(T value) {
+        this.head = new Node<>(value);
+        size = 1;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0 && this.head == null;
+    }
 
     private class Node<T> {
         private T value;
