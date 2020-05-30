76e1f4bce580d5d1d469616c917e8eefa7f614af
Grant Kang <3s.grantkang@gmail.com>
2020-05-30 11:50:19 -0700

    created a inner class Node that the outer SinglyLinkedList class will use

---
+++ src/linked_lists/SinglyLinkedList.java
@@ -0,0 +1,30 @@
+package linked_lists;
+
+public class SinglyLinkedList {
+
+    private class Node<T> {
+        private T value;
+        private Node<T> next;
+
+        public Node(T value) {
+            this.value = value;
+            this.next = null;
+        }
+
+        public T getValue() {
+            return value;
+        }
+
+        public void setValue(T value) {
+            this.value = value;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+    }
+}
